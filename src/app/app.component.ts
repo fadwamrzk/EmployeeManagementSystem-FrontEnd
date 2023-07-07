7050cdab7f24cee4abc1514e36b7c8576701ba1c
Fadwa Marzouk <fadwafadwamarzouk123@gmail.com>
2023-07-07 20:04:42 +0100

    added changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent implements OnInit {
   public employees: Employee[] = [];
   public editEmployee!: Employee | null ;
+  public deleteEmployee!: Employee | null ;
   constructor (private employeeService: EmployeeService){}
 
   ngOnInit() {
@@ -48,7 +49,7 @@ export class AppComponent implements OnInit {
 
   public onUpdateEmployee(employee : Employee): void{
     
-    this.employeeService.addEmployee(employee).subscribe(
+    this.employeeService.updateEmployee(employee).subscribe(
       (response: Employee) => {
         console.log(response);
         this.getEmployees();
@@ -56,9 +57,38 @@ export class AppComponent implements OnInit {
       (error: HttpErrorResponse) => {
         alert(error.message);
       }
-
     );
+  }
 
+  public onDeleteEmployee(employeeId : number ): void{
+    if(employeeId!=null){
+      this.employeeService.deleteEmployee(employeeId).subscribe(
+        (response: void) => {
+          console.log(response);
+          this.getEmployees();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
+    }
+
+  }
+  public searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
   }
 
   public onOpenModal(employee: Employee | null, mode: string): void{
@@ -79,10 +109,12 @@ export class AppComponent implements OnInit {
   }
 
   if(mode==='delete')
-  {
+  {this.deleteEmployee = employee;
     button.setAttribute('data-target','#deleteEmployeeModal');
   }
   container?.appendChild(button);
   button.click();
   }
+
+
 }
